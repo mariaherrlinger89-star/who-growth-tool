55463b0f79b9e2ae7c4eb6460677473a7bb0e783
mariaherrlinger89-star <maria.herrlinger89@gmail.com>
2025-09-10 22:44:13 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // <-- Alias für "@/..."
+    },
+  },
   build: { outDir: "dist" },
-  server: { port: 5173 }
+  server: { port: 5173 },
 });
